bec680807ac2237ccfad8ce74ee8200b4da76cfc
Philpax <me@philpax.me>
2025-03-31 02:44:38 +0200

    refactor: markdown -> html

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import Reveal from "reveal.js";
-import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 import RevealNotes from "reveal.js/plugin/notes/notes.esm.js";
 import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm.js";
 
 Reveal.initialize({
-  plugins: [Markdown, RevealNotes, RevealHighlight],
+  plugins: [RevealNotes, RevealHighlight],
   transition: "none",
   hash: true,
   width: 1280,
